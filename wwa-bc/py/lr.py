cd1fcb5e20d2dd48ec5281629bd621a62e73ad1e
wwagner4 <wwagner4@gmail.com>
2019-04-14 10:28:30 +0200

    added all combinations refactoring

--- wwa-bc/py/lr.py
+++ wwa-bc/py/lr.py
@@ -1,24 +1,22 @@
-from typing import Dict, List
+import itertools as it
+from typing import Dict, List, Iterable, Tuple
 
+import pandas as pd
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as Lda
 # from sklearn.lda import LDA # for older versions
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model.base import LinearClassifierMixin
-import pandas as pd
-import itertools as it
+import sklearn.model_selection as ms
 
 
-# feature_names = list(map(lambda b: b + "_" + grp, features_base))
-
-
-def extract_features(d: pd.DataFrame, features: List[str]) -> pd.DataFrame:
-    return d[features]
-
-
-def all_combis(li: List):
+def all_combis(li: List) -> Iterable[List]:
     for l in range(len(li)):
         for i in it.combinations(li, l + 1):
-            yield list(i)
+            yield i
+
+
+def extract_features(d: pd.DataFrame, features: Iterable[str]) -> pd.DataFrame:
+    return d[features]
 
 
 def extract_labels(d: pd.DataFrame):
@@ -26,36 +24,13 @@ def extract_labels(d: pd.DataFrame):
     return dia.values.ravel()
 
 
-def train_fs(data: pd.DataFrame, features_key: str, features: List[str], clf_key, clf):
-    rows = data.shape[0]
-    ntest = int(rows * 0.2)
-    data_te = data[:ntest]
-    data_tr = data[ntest:]
-    x_tr = extract_features(data_tr, features)
-    x_te = extract_features(data_te, features)
-    y_tr = extract_labels(data_tr)
-    y_te = extract_labels(data_te)
+def train_fs(data: pd.DataFrame, features_key: str, features: Iterable[str], clf_key, clf):
+    x = extract_features(data, features)
+    y = extract_labels(data)
+    x_tr, x_te, y_tr, y_te = ms.train_test_split(x, y)
     clf: LinearClassifierMixin = clf().fit(x_tr, y_tr)
     s = clf.score(x_te, y_te)
     print("{:10} {:20} {:10.3f}".format(clf_key, features_key, s))
-
-
-clfs = {
-    "lr": lambda: LogisticRegression(solver='lbfgs', max_iter=1500),
-    "lda": lambda: Lda(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False,
-                       tol=0.0001),
-}
-
-feature_groups_base = ["mean", "se", "worst"]
-feature_groups = all_combis(feature_groups_base)
-
-feature_base_selection: Dict[str, List[str]] = {
-    "all": ["radius", "texture", "perimeter", "area", "smoothness", "compactness", "concavity", "concave_points",
-            "symmetry", "fractal_dimension"],
-    "most_rel": ["radius", "texture", "compactness", "concavity", "concave_points"],
-    "two_rel": ["radius", "concavity"],
-    "one_rel": ["radius"],
-}
 
 
 def feature_names(grps: List[str], bases: List[str]) -> List[str]:
@@ -66,23 +41,45 @@ def feature_names(grps: List[str], bases: List[str]) -> List[str]:
     return re
 
 
-def feature_keys(grps: List[str], base: str, all_grps: bool) -> str:
+def feature_keys(grps: Iterable[str], base: str, all_grps: bool) -> str:
     if base == "all" and all_grps:
         return "all"
     if all_grps:
-        return "all_{}".format(sel_key)
-    return "{}_{}".format('_'.join(grps), sel_key)
+        return "all_{}".format(base)
+    return "{}_{}".format('_'.join(grps), base)
 
 
-feature_sets = {}
-for _grps in feature_groups:
-    for sel_key in feature_base_selection.keys():
-        key = feature_keys(_grps, sel_key, len(_grps) == len(feature_groups_base))
-        val = feature_names(_grps, feature_base_selection[sel_key])
-        feature_sets[key] = val
+def fs(grp_combis: Iterable[List[str]], base_sel: Dict[str, Iterable[str]], len_grps_base) -> Dict[str, Iterable[str]]:
+    feature_sets = {}
+    for grps in grp_combis:
+        for sel_key in base_sel.keys():
+            key = feature_keys(grps, sel_key, len(grps) == len_grps_base)
+            val = feature_names(grps, feature_selections[sel_key])
+            feature_sets[key] = val
+    return feature_sets
+
+
+clfs = {
+    "lr": lambda: LogisticRegression(solver='lbfgs', max_iter=4000),
+    "lda": lambda: Lda(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False,
+                       tol=0.0001),
+}
+
+feature_groups = ["mean", "se", "worst"]
+
+feature_selections: Dict[str, List[str]] = {
+    "all": ["radius", "texture", "perimeter", "area", "smoothness", "compactness", "concavity", "concave_points",
+            "symmetry", "fractal_dimension"],
+    "most_rel": ["radius", "texture", "compactness", "concavity", "concave_points"],
+    "two_rel": ["radius", "concavity"],
+    "one_rel": ["radius"],
+}
 
 _data = pd.read_csv('../data/bc-data.csv', header=0)
 
+feature_group_combis = all_combis(feature_groups)
+_feature_sets = fs(feature_group_combis, feature_selections, len(feature_groups))
+
 for _clf_key in clfs:
-    for _features_key in feature_sets.keys():
-        train_fs(_data, _features_key, feature_sets[_features_key], _clf_key, clfs[_clf_key])
+    for _features_key in _feature_sets.keys():
+        train_fs(_data, _features_key, _feature_sets[_features_key], _clf_key, clfs[_clf_key])
