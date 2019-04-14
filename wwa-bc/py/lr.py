088c0ed1b77e97d8c12f6635623132d2172eafd9
wwagner4 <wwagner4@gmail.com>
2019-04-14 07:52:52 +0200

    added all features

--- wwa-bc/py/lr.py
+++ wwa-bc/py/lr.py
@@ -1,15 +1,17 @@
 from typing import Dict, List
 
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as Lda
 # from sklearn.lda import LDA # for older versions
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model.base import LinearClassifierMixin
 import pandas as pd
 
 
-def extract_features(d: pd.DataFrame, features_base: List[str], grp: str) -> pd.DataFrame:
-    feature_names = list(map(lambda b: b + "_" + grp, features_base))
-    return d[feature_names]
+# feature_names = list(map(lambda b: b + "_" + grp, features_base))
+
+
+def extract_features(d: pd.DataFrame, features: List[str]) -> pd.DataFrame:
+    return d[features]
 
 
 def extract_labels(d: pd.DataFrame):
@@ -17,47 +19,57 @@ def extract_labels(d: pd.DataFrame):
     return dia.values.ravel()
 
 
-def train_fs(features_key: str, features_base: List[str], features_grp: str, clf_key, clf):
-    """
-    Trainiert einen Classifier für ein feature set
-    """
-    X_tr = extract_features(_data_tr, features_base, features_grp)
-    X_te = extract_features(_data_te, features_base, features_grp)
-    y_tr = extract_labels(_data_tr)
-    y_te = extract_labels(_data_te)
-    clf: LinearClassifierMixin = clf().fit(X_tr, y_tr)
-    s = clf.score(X_te, y_te)
-    print("clf:{:10} grp:{:10} fs:{:10} score:{:10}".format(clf_key, features_grp, features_key, s))
+def train_fs(data: pd.DataFrame, features_key: str, features: List[str], clf_key, clf):
+    rows = data.shape[0]
+    ntest = int(rows * 0.2)
+    data_te = data[:ntest]
+    data_tr = data[ntest:]
+    x_tr = extract_features(data_tr, features)
+    x_te = extract_features(data_te, features)
+    y_tr = extract_labels(data_tr)
+    y_te = extract_labels(data_te)
+    clf: LinearClassifierMixin = clf().fit(x_tr, y_tr)
+    s = clf.score(x_te, y_te)
+    print("{:10} {:20} {:10.3f}".format(clf_key, features_key, s))
 
-
-_data = pd.read_csv('../data/bc-data.csv', header=0)
-rows = _data.shape[0]
-ntest = int(rows * 0.2)
-
-_data_te = _data[:ntest]
-_data_tr = _data[ntest:]
 
 clfs = {
-    "lr": lambda: LogisticRegression(solver='lbfgs', max_iter=500),
-    "lda_1": lambda: LDA(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False,
+    "lr": lambda: LogisticRegression(solver='lbfgs', max_iter=1500),
+    "lda_1": lambda: Lda(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False,
                          tol=0.0001),
-    "lda_2": lambda: LDA(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False,
+    "lda_2": lambda: Lda(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False,
                          tol=0.001),
-    "lda_3": lambda: LDA(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False,
+    "lda_3": lambda: Lda(n_components=None, priors=None, shrinkage=None, solver='svd', store_covariance=False,
                          tol=0.01),
-}
-
-feature_sets: Dict[str, List[str]] = {
-    "all": ["radius", "texture", "perimeter", "area", "smoothness", "compactness",
-            "concavity", "concave_points", "symmetry", "fractal_dimension"],
-    "most_rel": ["radius", "texture", "compactness", "concavity", "concave_points"],
-    "two_rel": ["radius", "concavity"],
-    "radius": ["radius"],
 }
 
 feature_groups = ["mean", "se", "worst"]
 
-for clf_key in clfs:
-    for fg in feature_groups:
-        for fs_key in feature_sets.keys():
-            train_fs(fs_key, feature_sets[fs_key], fg, clf_key, clfs[clf_key])
+feature_base_selection: Dict[str, List[str]] = {
+    "all": ["radius", "texture", "perimeter", "area", "smoothness", "compactness", "concavity", "concave_points",
+            "symmetry", "fractal_dimension"],
+    "most_rel": ["radius", "texture", "compactness", "concavity", "concave_points"],
+    "two_rel": ["radius", "concavity"],
+    "one_rel": ["radius"],
+}
+
+feature_sets = {}
+for grp in feature_groups:
+    for sel_key in feature_base_selection.keys():
+        key = "{}_{}".format(grp, sel_key)
+        val = list(map(lambda b: b + "_" + grp, feature_base_selection[sel_key]))
+        feature_sets[key] = val
+
+fall = []
+ba = feature_base_selection["all"]
+for grp in feature_groups:
+    for f in ba:
+        fall.append("{}_{}".format(f, grp))
+
+feature_sets["all"] = fall
+
+_data = pd.read_csv('../data/bc-data.csv', header=0)
+
+for _clf_key in clfs:
+    for _features_key in feature_sets.keys():
+        train_fs(_data, _features_key, feature_sets[_features_key], _clf_key, clfs[_clf_key])
--- wwa-bc/py/tryout.py
+++ wwa-bc/py/tryout.py
@@ -1,8 +1,29 @@
-from collections import namedtuple
-H = namedtuple("H", "a b c")
-x = H(1, 2, True)
+from itertools import combinations
+from typing import List
 
-print(x.a)
-print(x.b)
-print(x.c)
 
+def named_tuples():
+    from collections import namedtuple
+    H = namedtuple("H", "a b c")
+    x = H(1, 2, True)
+    print(x.a)
+    print(x.b)
+    print(x.c)
+
+
+def all_combinations():
+    def all_combis(li: List[str]):
+        for l in range(len(li)):
+            for i in combinations(li, l + 1):
+                yield list(i)
+
+    # Print the obtained combinations
+    _li = ["A", "B", "C"]
+    ac = all_combis(_li)
+    for c in ac:
+        print(c)
+
+
+all_combinations()
+
+# named_tuples()
